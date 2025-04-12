9a4bbf5509a4c97dfb2dfc82e32f427701c900b0
Jitesh Yadav <jiteshyadav1990@gmail.com>
2025-04-12 13:34:13 -0400

    feat: health endpoint added

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EditorGateway } from './gateways/editor.gateway';
 import { SessionModule } from './modules/session.module';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [
@@ -10,7 +11,7 @@ import { SessionModule } from './modules/session.module';
     }),
     SessionModule,
   ],
-  controllers: [],
+  controllers: [HealthController],
   providers: [EditorGateway],
 })
 export class AppModule {}
